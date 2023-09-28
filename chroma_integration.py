1847f5502b670eb93dfc9a46f7ed072079db8274
Antoine <antoine1.anthony@gmail.com>
2023-09-28 08:48:36 -0700

    dev: added logs and error handling. updated gitignore

--- chroma_integration.py
+++ chroma_integration.py
@@ -31,70 +31,100 @@ EMBEDDING_MODEL = "text-embedding-ada-002"
 chroma_client = Client(Settings(anonymized_telemetry=False, allow_reset=True))
 embedding_function = OpenAIEmbeddingFunction(api_key=OPENAI_KEY, model_name=EMBEDDING_MODEL)
 
-
 def create_chroma_collection(collection_name: str) -> Client:
-    """Create or get a collection in Chroma."""
-    collection = chroma_client.get_or_create_collection(collection_name, embedding_function=embedding_function)
-    logging.info("Chroma Collection created or retrieved! The collection name is: %s", collection_name)
-    return collection
+    try:
+        logging.info(f'Creating or retrieving Chroma collection: {collection_name}')
+        print(f'Creating or retrieving Chroma collection: {collection_name}')
+        collection = chroma_client.get_or_create_collection(collection_name, embedding_function=embedding_function)
+        logging.info(f'Chroma Collection created or retrieved! The collection name is: {collection_name}')
+        print(f'Chroma Collection created or retrieved! The collection name is: {collection_name}')
+        return collection
+    except Exception as e:
+        logging.error(f"Error in create_chroma_collection: {e}")
+        print(f"Error: {e}")
+        return None  # Return None in case of an error
+
 
 def add_embeddings_to_chroma(df: pd.DataFrame, collection: Client):
-    """Add embeddings from the dataframe to Chroma."""
     try:
-        # Extract embeddings, documents, and ids
+        logging.info('Adding embeddings to Chroma.')
+        print('Adding embeddings to Chroma.')
         embeddings = df["embeddings"].tolist()
         documents = df["text"].tolist()
-
-        # Convert the range of integers to a list of strings
         ids = [str(i) for i in range(len(df))]
-
-        # Add to Chroma collection
         collection.add(embeddings=embeddings, documents=documents, ids=ids)
-        logging.info("Embeddings added to Chroma.")
+        logging.info('Embeddings added to Chroma.')
+        print('Embeddings added to Chroma.')
     except Exception as e:
-        logging.error(f"Error adding embeddings to Chroma: {e}")
+        logging.error(f"Error in add_embeddings_to_chroma: {e}")
+        print(f"Error: {e}")
+
 
 def index_data_to_chroma(df: pd.DataFrame, collection_name: str = "default_collection"):
-
-    """Index processed data to ChromaDB."""
-    # Create or get the specified collection
-    collection = create_chroma_collection(collection_name)
-
-    # add_embeddings_to_chroma(df, collection)
-    # logging.info("Data indexed to ChromaDB successfully!")
-
     try:
+        logging.info(f'Indexing data to Chroma with collection name: {collection_name}')
+        print(f'Indexing data to Chroma with collection name: {collection_name}')
+        collection = create_chroma_collection(collection_name)
         df["embeddings"] = df.text.apply(lambda x: openai.Embedding.create(input=x, engine="text-embedding-ada-002")["data"][0]["embedding"])
-
         add_embeddings_to_chroma(df, collection)
-        logging.info("Data indexed to ChromaDB successfully!")
+        logging.info('Data indexed to ChromaDB successfully!')
+        print('Data indexed to ChromaDB successfully!')
     except Exception as e:
-        logging.error(f"Error creating embeddings: {e}")
-
-def create_context(collection_name: str, question: str, max_len: int = 1800) -> Optional[str]:
-
-    collection =  chroma_client.get_collection(name=collection_name, embedding_function=embedding_function)
-    results = query_chroma_collection(collection, question, n_results=5)
-    
-
-    # For now, let's just return None since the documents are not available
-    if not results.get("documents"):
-        return None
-
-    # If the documents were available, the below line would extract them
-    contexts = results["documents"]
-
-    flattened_contexts = [doc for sublist in contexts for doc in sublist]
-    return "\n\n###\n\n".join(flattened_contexts) if flattened_contexts else None
+        logging.error(f"Error in index_data_to_chroma: {e}")
+        print(f"Error: {e}")
 
 
 def query_chroma_collection(collection: Client, question: str, n_results: int = 5) -> List[dict]:
-    """Query a Chroma collection and return the results."""
-    results = collection.query(
-        query_texts=[question],
-        n_results=n_results,
-        include=["distances", "documents"],
-    )
-    logging.debug(f"Chroma Query Results: {results}")
-    return results
+    try:
+        logging.info(f'Querying Chroma collection with question: {question}')
+        print(f'Querying Chroma collection with question: {question}')
+        results = collection.query(
+            query_texts=[question],
+            n_results=n_results,
+            include=["distances", "documents"],
+        )
+        logging.debug(f'Chroma Query Results: {results}')
+        print(f'Chroma Query Results: {results}')
+        return results
+    except Exception as e:
+        logging.error(f"Error in query_chroma_collection: {e}")
+        print(f"Error: {e}")
+        return []  # Return an empty list in case of an error
+
+
+def create_context(collection_name: str, question: str, max_len: int = 1800) -> Optional[str]:
+    try:
+        logging.info(f'Entering create_context with collection_name: {collection_name}, question: {question}, max_len: {max_len}')
+        print(f'Entering create_context with collection_name: {collection_name}, question: {question}, max_len: {max_len}')
+
+        collection = chroma_client.get_collection(name=collection_name, embedding_function=embedding_function)
+        logging.info('Retrieved collection from chroma_client.')
+        print('Retrieved collection from chroma_client.')
+
+        results = query_chroma_collection(collection, question, n_results=5)
+        logging.info(f'Query results: {results}')
+        print(f'Query results: {results}')
+
+        # For now, let's just return None since the documents are not available
+        if not results.get("documents"):
+            logging.warning('No documents available in query results.')
+            print('No documents available in query results.')
+            return None
+
+        # If the documents were available, the below line would extract them
+        contexts = results["documents"]
+        logging.info(f'Contexts extracted: {contexts}')
+        print(f'Contexts extracted: {contexts}')
+
+        flattened_contexts = [doc for sublist in contexts for doc in sublist]
+        final_context = "\n\n###\n\n".join(flattened_contexts) if flattened_contexts else None
+        logging.info(f'Final context: {final_context}')
+        print(f'Final context: {final_context}')
+
+        return final_context
+
+    except Exception as e:
+        logging.error(f"Error in create_context: {e}")
+        print(f"Error: {e}")
+        return None  # Return None in case of an error
 
